6cf1d015ed687994d701606a1239185e75432f65
Youssefrafat12 <youssefrafat991@gmail.com>
2023-07-13 08:53:42 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
- 
-/**
+ /**
  * main - prints  "Programming is like building a multilingual
  * puzzle, followed by a newline."
  * return 0.
@@ -13,7 +12,7 @@
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts( "\"Programming is like building a multilingual puzzle");
 
 return(0);
 }
